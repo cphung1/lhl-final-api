fbffe868d9eb7b56f941bc2f75c05173dbb9cd4c
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-11 21:58:47 +0000

    rendered seeded events data as json

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
   Event.create(
-    name: "Electric Daisy Carnvial 2020", details: ""
+    name: "Electric Daisy Carnvial 2020", details: "Commonly known as EDC, is the largest electronic dance music festival in North America. Held at the Las Vegas Motor Speedway. EDC was created with the purpose of utilizing music and art to inspire individuals.", start_date: "Oct 2, 2020", end_date: "Oct 4, 2020", location: "Las Vegas, NV"
   )
