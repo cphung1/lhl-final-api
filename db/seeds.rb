c2f700b8f24b6dd39204828ca58ecb97a2b372cf
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-12 23:04:09 +0000

    added more event seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
   Event.create(
-    name: "Electric Daisy Carnvial 2020", details: "Commonly known as EDC, is the largest electronic dance music festival in North America. Held at the Las Vegas Motor Speedway. EDC was created with the purpose of utilizing music and art to inspire individuals.", start_date: "Oct 2, 2020", end_date: "Oct 4, 2020", location: "Las Vegas, NV"
+    name: "Electric Daisy Carnvial", details: "Commonly known as EDC, is the largest electronic dance music festival in North America. Held at the Las Vegas Motor Speedway. EDC was created with the purpose of utilizing music and art to inspire individuals.", start_date: "Oct 2, 2020", end_date: "Oct 4, 2020", location: "Las Vegas, NV, USA"
   )
 
   Event.create(
-    name: "FVDED In the Park", details: "Western Canada's largest urban music festival promises an edgy, high-caliber lineup and state-of-the-art stages for this annual two day festival. Held at Surrey's Holland Park just outside Vancouver", start_date: "July 10, 2020", end_date: "July 11, 2020", location: "Vancouver, BC"
+    name: "FVDED In the Park", details: "Western Canada's largest urban music festival promises an edgy, high-caliber lineup and state-of-the-art stages for this annual two day festival. Held at Surrey's Holland Park just outside Vancouver", start_date: "July 10, 2020", end_date: "July 11, 2020", location: "Vancouver, BC, Canada"
   )
+
+  Event.create(
+    name: "Shambhala Music Festival", details: "Shambhala is electronic music festival in Canada that have knee-trembling lineups are stacked with awesome artists. Those craving the vibrations of the lower frequencies will have more than enough music to choose from. Shambhala lives by one principle: 'It’s all about the people on the dancefloor.'", start_date: "July 24, 2020", end_date: "July 27, 2020", location: "Salmo, BC, Canada"
+  )
+
+  Event.create(
+    name: "Tomorrowland", details: "A Belgian electronic dance music festival held in Boom, Belgium. Tomorrowland was first held in 2005. It now stretches over 2 weekends and usually sells out in minutes.", start_date: "July 17, 2020", end_date: "July 26, 2020", location: "Boom, Antwerp, Belgium"
+  )
+
+  Event.create(
+    name: "Chasing Summer", details: "The largest outdoor EDM event in Western Canada returns to Alberta for this two-day annual electronic fest in August. Take advantage of the last days of summer to party at the Max Bell Centre and dance from sunrise to sundown with this jam-packed lineup.", start_date: "Aug 1, 2020", end_date: "Aug 2, 2020", location: "Calgary, AB, Canada"
+  )
+
+  Event.create(
+    name: "Ultra Music Festival", details: "Commonly known as UMF, is an annual outdoor electronic music festival that is primarily been held at Bayfront Park in downtown Miami. It was founded in 1999 by Russell Faibisch and Alex Omes and is named after the 1997 Depeche Mode album, Ultra.", start_date: "Mar 26, 2021", end_date: "Mar 28, 2021", location: "Miami, Florida, USA"
+  )
+
+  Event.create(
+    name: "Ultra Music Festival", details: "Commonly known as UMF, is an annual outdoor electronic music festival that is primarily been held at Bayfront Park in downtown Miami. It was founded in 1999 by Russell Faibisch and Alex Omes and is named after the 1997 Depeche Mode album, Ultra.", start_date: "Mar 26, 2021", end_date: "Mar 28, 2021", location: "Miami, Florida, USA"
+  )
+  
