e3a0b0b6c4c240cb4b1a67c2a460720b00e3f760
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-04-16 01:17:22 +0000

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,11 @@ class UsersController < ApplicationController
    # @single_event_with_users = UserEvent.where(event_id: @event_id).pluck(:user_id)
     #@all_users_to_event = User.find(@single_event_with_users)
     
-    #@dulpicate = @all_users_to_event.where.not(user_id: @user_id)
+    #@dulpicate = @all_users_to_event.where.not(id: @user_id)
 
-    @single_event_with_users = UserEvent.where(event_id: @event_id).map(&:single_event_with_users)
-    @testing = User.find(@single_event_with_users).where.not(id: 1)
-
-    render json: @testing
+    #@single_event_with_users = UserEvent.where(event_id: @event_id).map(&:single_event_with_users)
+    
+    #render json: @testing
 
 
     #render json: @all_users_to_event
