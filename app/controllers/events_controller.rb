a6051f0a547bf24cf0264b9134fa69b3d1fbe5f8
cphung1 <cphung97@gmail.com>
2020-04-16 22:31:04 +0000

    route for single event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,11 @@
 class EventsController < ApplicationController
   def index
-    events = Event.all
-    render json: events
+    @events = Event.all
+    render json: @events
   end
 
   def show
+    @event = Event.where(id: params[:id])
+    render json: @event
   end
 end
